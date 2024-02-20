da3c969a289da8bdb0bbce8300dffe39ada7fcef
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2024-02-20 01:06:11 +0000

    :memo: Controller Input

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,49 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import initialAnimes from "./animes.json"
 
 //create your first component
 const Home = () => {
+
+	const [ searchValue, setSearchValue ] = useState("")
+
+	const [ animes, setAnimes ] = useState(initialAnimes)
+
+	const deleteAnime = (animeName) =>{
+
+		const animesFiltered = animes.filter( itm => itm.title.text != animeName )
+		setAnimes(animesFiltered)
+
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">
+				{ searchValue == '' ? 'Search Animes!' : `Searching for ${searchValue}`}
+			</h1>
+
+			<input 
+				className="my-2" type="text" placeholder="search" defaultValue={searchValue}
+				onKeyUp={(evt) => { 
+					console.log(evt)
+					if(evt.key == 'Enter'){
+						setAnimes([ { title: { text: evt.target.value }} , ...animes ])
+					}else{
+						setSearchValue(evt.target.value)
+					}
+				}} 
+			/>
+
+			{
+				animes.filter(item => item.title.text.toLocaleLowerCase()
+					.includes(searchValue.toLocaleLowerCase()) 
+				).map((serie, ind)=> <div key={ind} className="bg-light rounded-4 mx-2" 
+					onClick={() => deleteAnime(serie.title.text)}
+					>
+					<p>{serie.title.text}</p>
+					<p>{serie.studio}</p>
+				</div>)
+			}
+
 		</div>
 	);
 };
